099aeda8a9f7883699f085a0b704f00ac54d4a9d
Romancho <dev@romancho.pro>
2021-11-06 16:57:35 +0300

    Fix .env

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
-<?php require_once( __DIR__ .'/vendor/autoload.php' );
-$dotenv = Dotenv\Dotenv::createImmutable( __DIR__ );
+<?php require_once( __DIR__ . '/vendor/autoload.php' );
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
 
@@ -7,11 +7,11 @@ $dotenv->load();
 <html>
     <head>
         <title></title>
-        <link type="text/css" rel="stylesheet" href="<?= getenv('ASSETS_PATH')?>/css/style<?= getenv('APP_ENV') === 'prod' ? '.min' : null?>.css"/>
+        <link type="text/css" rel="stylesheet" href="<?= $_SERVER['ASSETS_PATH']?>/css/styles<?= $_SERVER['APP_ENV'] === 'prod' ? '.min' : null?>.css"/>
     </head>
     <body>
-    <script src="<?= getenv('ASSETS_PATH')?>/js/vendor<?= getenv('APP_ENV') === 'prod' ? '.min' : null?>.css"></script>
-    <script src="<?= getenv('ASSETS_PATH')?>/js/app<?= getenv('APP_ENV') === 'prod' ? '.min' : null?>.css"></script>
+    <script src="<?= $_SERVER['ASSETS_PATH']?>/js/vendor<?= $_SERVER['APP_ENV'] === 'prod' ? '.min' : null?>.js"></script>
+    <script src="<?= $_SERVER['ASSETS_PATH']?>/js/app<?= $_SERVER['APP_ENV'] === 'prod' ? '.min' : null?>.js"></script>
     <script>
         $(function(){
             DefaultPage.init();
